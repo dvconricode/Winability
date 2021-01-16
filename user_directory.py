0dbdecce31b1bf9b7a4bb8fcb625ede51ce51f77
Love <love.chien1112@gmail.com>
2021-01-16 10:40:48 -0800

    finds out user OS and creates filepath to the user desktop

---
+++ user_directory.py
@@ -0,0 +1,12 @@
+
+import os
+import sys
+
+if sys.platform.startswith('win32'):
+    username = os.getlogin()
+    file = open(f'C:\\Users\\{username}\\Desktop\\stock_tickers.txt')
+    file.close()
+
+elif sys.platform.startswith('darwin'):
+    file = open(os.path.expanduser("~/Desktop/stock_tickers.txt"))
+    file.close()
