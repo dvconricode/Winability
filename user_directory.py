7b1eaa1641ec6f4444bc82137fba081d3a988595
Love <love.chien1112@gmail.com>
2021-01-16 12:40:58 -0800

    created function to add 3 subfolders to main StockData folder

--- user_directory.py
+++ user_directory.py
@@ -11,13 +11,18 @@ def create_folders_by_system():
         create_new_desktop_folder_mac()
 
 
-# the following creates new folder on user desktop for windows OS
+# the following creates new folder and its subfolders on user desktop for windows OS
 def create_new_desktop_folder_windows():
-    if sys.platform.startswith('win32'):
-        desktop_path = os.path.join(os.environ['USERPROFILE'], 'Desktop')
-        new_path = desktop_path + r'\StockData'
-        if not os.path.exists(new_path):
-            os.makedirs(new_path)
+
+    desktop_path = os.path.join(os.environ['USERPROFILE'], 'Desktop')
+    new_path = desktop_path + r'\StockData'
+    if not os.path.exists(new_path):
+        os.makedirs(new_path)
+    else:
+        print('File already exists')
+
+    create_subfolders(new_path)
+
 
 # the following creates new folder on user desktop for mac OS
 def create_new_desktop_folder_mac():
@@ -30,7 +35,14 @@ def create_new_desktop_folder_mac():
         print("Created")
 
 
+def create_subfolders(path):
+    subfolders = [r'\OHLC', r'\Earnings', r'\Merged']
 
+    for f in subfolders:
+        if not os.path.exists(path + f):
+            os.makedirs(path + f)
+        else:
+            print(f[1:] + ' already exists')
 
 
 create_folders_by_system()
