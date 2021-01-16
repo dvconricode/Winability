23ce4cba91a98dcf05de1252623434220eaec925
Love <love.chien1112@gmail.com>
2021-01-16 11:29:04 -0800

    deleted .csv creating function

--- user_directory.py
+++ user_directory.py
@@ -2,19 +2,6 @@
 import os
 import sys
 
-# the following function creates new .csv file on user desktop, Windows OS and MacOS
-
-def create_new_csv():
-
-    if sys.platform.startswith('win32'):
-        username = os.getlogin()
-        file = open(f'C:\\Users\\{username}\\Desktop\\stock_tickers.csv', 'x')
-        file.close()
-
-    elif sys.platform.startswith('darwin'):
-        file = open(os.path.expanduser('~/Desktop/stock_tickers.csv'), 'x')
-        file.close()
-
 
 # the following if creates new folder on user desktop for windows OS
 
