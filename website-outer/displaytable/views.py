5cca7fbd18d0a1db98f7c8ad6a814fd124d5526b
Richard-Ge <61361227+Richard-Ge@users.noreply.github.com>
2021-01-16 13:38:34 -0800

    Add the other files
    important: views.py will have the functional python code. 
    that will output a Context object to templates/displaytable/index.html, which will be able to work with Jasper's original program output.
    //TODO: later, use browser cookies in addition to .csv's on disk?

---
+++ website-outer/displaytable/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponse
+from django.template import loader
+
+import time
+
+# Create your views here.
+
+def index(request):
+	# DEBUG return HttpResponse("Index page <p>{% print(1) %}</p>")
+	# DEBUG2 output = " | ".join(x*x for x in [1,2,3])
+	# DEBUG2 return HttpResponse(output)
+	
+	timetemp = time.time()
+	ticker_data = [timetemp, ["AAPL",39.91,67,33], ["GOOG",154.12,43.1,56.9], ["Third", 31.18, 90, 10]]
+
+	template = loader.get_template('displaytable/index.html')
+	context = {"ticker_data":ticker_data}
+	return HttpResponse(template.render(context, request))
