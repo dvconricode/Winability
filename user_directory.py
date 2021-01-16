284741036683cf90602c8cf47ed68a576c3b41b4
Love <love.chien1112@gmail.com>
2021-01-16 10:47:17 -0800

    fixed issue where file was not created. changed created file from .txt to .csv

--- user_directory.py
+++ user_directory.py
@@ -4,9 +4,9 @@ import sys
 
 if sys.platform.startswith('win32'):
     username = os.getlogin()
-    file = open(f'C:\\Users\\{username}\\Desktop\\stock_tickers.txt')
+    file = open(f'C:\\Users\\{username}\\Desktop\\stock_tickers.csv', 'x')
     file.close()
 
 elif sys.platform.startswith('darwin'):
-    file = open(os.path.expanduser("~/Desktop/stock_tickers.txt"))
+    file = open(os.path.expanduser('~/Desktop/stock_tickers.csv'), 'x')
     file.close()
