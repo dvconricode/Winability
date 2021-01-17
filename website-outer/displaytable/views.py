4435ad04f06461fe6484d85df7e77fa05e9b0561
Richard-Ge <61361227+Richard-Ge@users.noreply.github.com>
2021-01-17 13:30:18 -0800

    Update views.py

--- website-outer/displaytable/views.py
+++ website-outer/displaytable/views.py
@@ -5,16 +5,20 @@ import time
 
 from .forms import TickerForm
 
-# Create your views here.
+# Create your views here. 
+
+##############
+
+##############
+
+timetemp = time.asctime(time.localtime())  # updates every refresh
+other_data = [timetemp]
+ticker_data = [["AAPL",39.91,67,33], ["GOOG",154.12,43.1,56.9], ["Third", 31.18, 90, 10]]
 
 def index(request):
 	# DEBUG return HttpResponse("Index page <p>{% print(1) %}</p>")
 	# DEBUG2 output = " | ".join(x*x for x in [1,2,3])
 	# DEBUG2 return HttpResponse(output)
-
-	timetemp = time.asctime(time.localtime())
-	other_data = [timetemp]
-	ticker_data = [["AAPL",39.91,67,33], ["GOOG",154.12,43.1,56.9], ["Third", 31.18, 90, 10]]
 
 	template = loader.get_template('displaytable/index.html')
 	context = {"other_data":other_data, "ticker_data":ticker_data}
@@ -27,7 +31,14 @@ def get_ticker(request):
 			form = TickerForm(request.POST)
 			if True:
 				template = loader.get_template('displaytable/search.html')
-				context= {'ticker': request.POST['ticker']} #gotta get the ticker from the form
+				context = {'ticker': request.POST['ticker']} #gotta get the ticker from the form
+				my_search = context['ticker']  # the ticker we search for
+				results_list = []
+				for each_list in ticker_data:
+					if my_search in each_list[0]:
+						results_list.append(each_list)
+				if results_list != []:  # 
+					context.update({"ticker_data":results_list})
 				return HttpResponse(template.render(context, request))
 			#print(form.errors)
 			#return HttpResponse('test')
